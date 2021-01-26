8926adfa5cc36ea117db669312b60c227c11fe3b
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-01-26 18:16:13 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Game from "./components/Game"
+
+
+const App = () => (
+  <div>
+    <Game />
+  </div>
+)
+
+export default App;
---
+++ helpers.js
@@ -0,0 +1,37 @@
+/*checks all combinations from lines array and if any of them has all the
+3 elements the same then it returns the same element as the winner (o or x)
+otherwise returns null*/
+
+
+export function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+
+
+
+/*
+//example:
+
+const squares = [
+    "O", null, null,
+    null, "O", "X",
+    'O', 'X', 'O'
+];
+*/
