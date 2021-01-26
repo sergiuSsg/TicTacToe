72bc500949341a84f9d1331ee56a5bd8b8658b3b
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-01-26 18:47:09 +0000

    Update Game.js

--- components/Game.js
+++ components/Game.js
@@ -48,16 +48,16 @@ const Game = () => {
         }
     }
 
-    useEffect(() => {
-        if (winner) setModalOpen(true)
-        gameWon()
-    }, [winner])
-
     const renderMoves = () => {
         return <button onClick={() => resetGame()} >
             Restart Game
         </button>
     }
+    
+    useEffect(() => {
+        if (winner) setModalOpen(true)
+        gameWon()
+    }, [winner])
 
     return (
         <>
@@ -83,167 +83,3 @@ const Game = () => {
 }
 
 export default Game
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* with history but not finished all */
-
-
-/*
-import React, { useState } from "react"
-import { calculateWinner } from "../helpers"
-import Board from "./Board"
-
-
-const Game = () => {
-    const [history, setHistory] = useState([Array(9).fill(null)])
-    const [stepNumber, setStepNumber] = useState(0)
-    const [xIsNext, setXisNext] = useState(true)
-    const winner = calculateWinner(history[stepNumber])
-
-
-    const handleClick = i => {
-        const timeInHistory = history.slice(0, stepNumber + 1)
-        const current = timeInHistory[stepNumber]
-        const squares = [...current]
-
-        // if user clicks an occupied square or if game is won return
-        if (winner || boardCopy[i]) return;
-        //if (winner || boardCopy(i)]) return
-        //put an X or O in the clicked square
-        squares[i] = xIsNext ? "X" : "O";
-        setHistory([...timeInHistory, squares])
-        setXisNext(!xIsNext)
-    }
-    const jumpTo = () => {
-
-    }
-
-    const renderMoves = () => {
-        return <button onClick={() => setBoard(Array(9).fill(null))} >
-            Start Game
-        </button>
-    }
-
-    return (
-        <>
-            <Board squares={board} onClick={handleClick} />
-            <div className="underBoard" >
-                <p>{winner ? "Winner: " + winner : "Next Player: " + (xIsNext ? "X" : "O")}</p>
-                {renderMoves()}
-            </div>
-
-        </>
-
-    )
-}
-
-export default Game
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-*/
