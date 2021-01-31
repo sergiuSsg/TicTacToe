273191d1af3d7aab8962d1c1dedba57d50f8431d
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-01-31 17:16:44 +0000

    Update PopUp.js

--- components/PopUp.js
+++ components/PopUp.js
@@ -5,28 +5,36 @@ import "../styles.css"
 Modal.setAppElement("#root")
 
 export default function PopUp(props) {
+    const {
+        modalOpen,
+        setModalOpen,
+        startGame,
+        resetGame,
+        setTurn,
+        xWins,
+        oWins,
+        winner,
+        draw
+    } = props
 
     function modalStart() {
         return (
-            <Modal
-                isOpen={props.modalOpen}
-                className="popup"
-            >
+            <Modal isOpen={modalOpen} className="popup">
                 <div className="grid-container">
                     <h3 className="title">Tic Tac Toe</h3>
-                    <div className="selectPlayer">Select Player</div>
+                    <h4 className="selectPlayer">Select Player</h4>
                     <button
                         className="x"
                         onClick={() => {
-                            props.setTurn("X")
-                            props.setModalOpen(false)
+                            setTurn("X")
+                            setModalOpen(false)
                         }}
                     >X</button>
                     <button
                         className="o"
                         onClick={() => {
-                            props.setTurn("O")
-                            props.setModalOpen(false)
+                            setTurn("O")
+                            setModalOpen(false)
                         }}
                     >O</button>
                 </div>
@@ -34,32 +42,51 @@ export default function PopUp(props) {
         )
     }
 
-    function modalEnd() {
+    function modalWin() {
         return (
-            <Modal
-                isOpen={props.modalOpen}
-                className="popup"
-            >
+            <Modal isOpen={modalOpen} className="popup">
                 <div className="grid-container">
-                    <h3 className="endWinner">{props.winner} won</h3>
-
+                    <h3 className="endWinner">{winner} won</h3>
+                    <h4 className="scoreEnd">X:&ensp;{xWins}&emsp;--&emsp;O:&ensp;{oWins}</h4>
                     <div className="continueButton">
                         <button onClick={() => {
-                            props.startGame()
-                            props.setModalOpen(false)
-
-                        }}>Continue</button>
+                            startGame()
+                            setModalOpen(false)
+                        }} className="generic-button">Continue</button>
                     </div>
                     <div className="resetButton">
                         <button onClick={() => {
-                            props.resetGame()
-                            props.setModalOpen(false)
-                        }}>Reset Score</button>
+                            resetGame()
+                            setModalOpen(false)
+                        }} className="generic-button">Restart Game</button>
                     </div>
                 </div>
             </Modal >
         )
     }
 
-    return props.winner ? modalEnd() : modalStart()
+    function modalDraw() {
+        return (
+            <Modal isOpen={modalOpen} className="popup">
+                <div className="grid-container">
+                    <h3 className="endWinner">DRAW!</h3>
+                    <h4 className="scoreEnd">X:&ensp;{xWins}&emsp;--&emsp;O:&ensp;{oWins}</h4>
+                    <div className="continueButton">
+                        <button onClick={() => {
+                            startGame()
+                            setModalOpen(false)
+                        }} className="generic-button">Continue</button>
+                    </div>
+                    <div className="resetButton">
+                        <button onClick={() => {
+                            resetGame()
+                            setModalOpen(false)
+                        }} className="generic-button">Restart Game</button>
+                    </div>
+                </div>
+            </Modal >
+        )
+    }
+
+    return winner ? modalWin() : draw ? modalDraw() : modalStart()
 } 
