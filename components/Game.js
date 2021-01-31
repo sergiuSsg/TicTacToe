028df46c3198e681190c3a031a8548176fcd54d8
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-01-31 17:13:51 +0000

    Update Game.js

--- components/Game.js
+++ components/Game.js
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from "react"
-import { calculateWinner } from "../helpers"
+import { calculateWinner } from "../CalculateWinner"
 import Board from "./Board"
 import PopUp from "./PopUp"
 import TopBar from "./TopBar"
-
+import BottomBar from "./BottomBar"
 
 const Game = () => {
     const [board, setBoard] = useState(Array(9).fill(null))
     const [modalOpen, setModalOpen] = useState(true)
-
     const [turn, setTurn] = useState("X")
     const [xWins, setXwins] = useState(0)
     const [oWins, setOwins] = useState(0)
-    //const [chooseX, setChooseX] = useState(false)
-    //const [chooseO, setChooseO] = useState(false)
     const winner = calculateWinner(board);
     const draw = (board.every(elem => elem !== null));
-
 
     const handleClick = (index) => {
         const boardCopy = [...board]
         if (winner || boardCopy[index]) return
-        //boardCopy[index] = xTurn ? "X" : "O";
         boardCopy[index] = turn;
         setBoard(boardCopy)
-        //setXturn(!xTurn)
         setTurn((turn) => turn === "X" ? "O" : "X")
     }
 
     const resetGame = () => {
+        setModalOpen(true)
         setXwins(0)
         setOwins(0)
         setBoard(Array(9).fill(null))
@@ -40,44 +35,35 @@ const Game = () => {
         setBoard(Array(9).fill(null))
     }
 
-    const gameWon = () => {
+    useEffect(() => {
+        if (winner) setModalOpen(true)
         if (winner === "X") {
             setXwins((wins) => wins + 1)
         } else if (winner === "O") {
             setOwins((wins) => wins + 1)
         }
-    }
-
-    const renderMoves = () => {
-        return <button onClick={() => resetGame()} >
-            Restart Game
-        </button>
-    }
-    
-    useEffect(() => {
-        if (winner) setModalOpen(true)
-        gameWon()
     }, [winner])
+
+    useEffect(() => {
+        if (draw) setModalOpen(true)
+    }, [draw])
 
     return (
         <>
             <PopUp
-                startGame={startGame}
-                setTurn={setTurn}
-                resetGame={resetGame}
-                turn={turn}
-                setModalOpen={setModalOpen}
                 modalOpen={modalOpen}
+                setModalOpen={setModalOpen}
+                startGame={startGame}
+                resetGame={resetGame}
+                setTurn={setTurn}
+                xWins={xWins}
+                oWins={oWins}
                 winner={winner}
+                draw={draw}
             />
             <TopBar xWins={xWins} oWins={oWins} />
-            <Board itemsArray={board} onClick={handleClick} />
-
-            <div className="underBoard" >
-                <p>{winner ? "Winner: " + winner : draw ? "Draw!!" : "Game in Progress..."}</p>
-                <p>{"Next Player: " + turn.toString()}</p>
-                {renderMoves()}
-            </div>
+            <Board itemsArray={board} handleClick={handleClick} />
+            <BottomBar resetGame={resetGame} turn={turn} />
         </>
     )
 }
