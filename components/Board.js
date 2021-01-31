802713ce24031cbafe3f8c1de49d5387645c85e3
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-01-31 17:10:20 +0000

    Update Board.js

--- components/Board.js
+++ components/Board.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Square from "./Square"
 
-const Board = ({ itemsArray, onClick }) => {
-    return (
-        <div className="board" >
-            {itemsArray.map((square, index) => (
-                <Square key={index} value={square} onClick={() => onClick(index)} />
-            ))}
-        </div>
-    )
-}
+const Board = ({ itemsArray, handleClick }) => (
+    <div className="board" >
+        {itemsArray.map((arrElement, index) => (
+            <Square key={index} value={arrElement} handleClick={() => handleClick(index)} />
+        ))}
+    </div>
+)
 
 export default Board
